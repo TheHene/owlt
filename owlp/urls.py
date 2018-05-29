c7f4eff9fca87f0116813b04311b7c1bf3052ef3
TheHene <hendrik.setzer@hotmail.de>
2018-05-29 22:32:11 +0200

    include semantic-ui

--- owlp/urls.py
+++ owlp/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('owt/', include('owt.urls')),
+    path('', include('owt.urls')),
     path('admin/', admin.site.urls),
 ]
